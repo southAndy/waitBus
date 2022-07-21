7dce7fade748a4c502e0007de1dc3c6afa8ba912
southAndy <zatchbell.1206@gmail.com>
2022-07-21 10:20:40 +0800

    feat:import pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
 
-createApp(App).use(Quasar, quasarUserOptions).use(router).mount("#app");
+createApp(App)
+  .use(Quasar, quasarUserOptions)
+  .use(router)
+  .use(createPinia())
+  .mount("#app");
---
+++ src/store/Bus/getNearStation.js
@@ -0,0 +1,39 @@
+import { getToken } from "@/service/getToken";
+import { defineStore } from "pinia";
+import leaflet from "leaflet";
+
+const nearbyStore = defineStore("nearbyStore", {
+  state: () => {
+    return {
+      apiToken: "",
+      //中心樣式
+      centerIcon: leaflet.icon({
+        iconUrl: require("@/assets/images/user-set.png"),
+        iconSize: [60, 60],
+      }),
+      //站牌icon
+      busIcon: leaflet.icon({
+        iconUrl: require("@/assets/images/icon/Component 4.png"),
+        iconSize: [25, 35],
+      }),
+      //站牌icon -clicked
+      pointedBusIcon: leaflet.icon({
+        iconUrl: require("@/assets/images/icon/Component 3.png"),
+        iconSize: [25, 35],
+      }),
+    };
+  },
+  getters: () => {},
+  actions: {
+    async getToken() {
+      this.apiToken = await getToken;
+      console.log(this.apiToken);
+    },
+    async getNearStation() {
+      await this.getToken();
+    },
+    //取得使用者定位
+  },
+});
+
+export default nearbyStore;
